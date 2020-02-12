26d26b7c3d01ca9bba1111693cb8bccd9ddc533a
Zancha Aguilar <zancha.aguilar@unc.edu.ph>
2020-02-12 20:09:09 +0800

    initial commit

---
+++ Aguilar_MT-Set-01/js/Option.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+function Option(value, uid) {
+    this.value = value;
+    this.uid = uid;
+    this.checked = false;
+}
+
+
+
+Option.prototype.render = function() {
+    var label = document.createElement("label"),
+        option = document.createElement("input"),
+        self = this;
+
+    option.value = this.value;
+    option.type = "radio";
+    option.name = this.uid;
+
+    option.addEventListener("change", function(e) {
+        self.checked = this.checked;
+    });
+
+    label.appendChild(option);
+    label.appendChild(document.createTextNode(option.value));
+
+    return label;
+};
+
+
+/**
+ * @return {string}
+ */
+Option.prototype.getValue = function() {
+    return this.value;
+};
+
+
+/**
+ * @return {boolean}
+ * ugly
+ */
+Option.prototype.isSelected = function() {
+    var choice = document.querySelector("input[name='" + this.uid + "']:checked");
+
+    return choice ? choice.value == this.value : false;
+};
---
+++ Aguilar_MT-Set-01/questions.js
@@ -0,0 +1,7 @@
+var questions = [
+    new Question("What is the capital of the Republic of the Philippines?", ["Manila", "Cebu", "Davao", "Baguio"], 0),
+    new Question("What is the Summer Capital of the Philippines?", ["Baguio City", "Camarines Sur", "Bohol", "Palawan"], 0),
+    new Question("Where is the highest peak in the Philippines?", ["Mt. Apo", "Mt. Pinatubo", "Mt. Pulag", "Mt. Province"], 0),
+    new Question("Where can you find Mayon Volcano?", ["Albay", "Camarines Sur", "Masbate", "Catanduanes"], 0),
+    new Question("What is the largest island of the Philippine Archipelago?", ["Luzon", "Mindanao", "Palawan", "Visayas"], 0)
+];
